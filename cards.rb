d7a20f06bb81134ee19eb3e926e1af1c29ef066d
clintwiley <cwiley@nfis.com>
2013-12-11 16:17:45 -0500

    added class 2 test

--- cards.rb
+++ cards.rb
@@ -1,11 +1,10 @@
 # Classes for OOP Blackjack
 
-
 # Define the cards we'll use in our game.
 class Cards
   attr_accessor :suit, :value
 
-  def initialize
+  def initialize(suit, value)
     @suit = %w(H D S C)
     @value = %w(2, 3, 4, 5, 6, 7, 8, 8, 10, J, Q, K, A)
   end
@@ -13,26 +12,60 @@ end
 
 # Define the deck of cards & shuffle the cards
 class Deck
-  attr_accessor :cards
+  attr_accessor :cards, :suit, :value
 
   def initialize
-    @cards << suit
-    @cards << value
+    @cards = []
+    @cards << Cards.new(suit, value)
     @shuffle = cards.shuffle!
   end
 end
 
-# Setup the initial hand and values.
-class Hand
+class Player
+  attr_accessor :name, :cards, :deck
+
+  def initialize(n)
+    @name = n
+  end
+
+end
+
+class Dealer
   attr_accessor :deck, :cards
 
-  def hand
-    2.times do
-      hand << deck.pop
-      puts "Your cards are #{total}"
+  def initialize
+    @dealer_name = 'Dealer'
+    @cards = Array.new
+  end
+end
+
+# Here are the main game play methods
+class GamePlay
+  attr_accessor :cards, :deck, :player, :dealer
+
+  def initialize
+    @deck = Deck.new
+    @player = Player.new("n")
+    @dealer = Dealer.new
+  end
+
+  def intro
+    puts ""
+    puts "=> Welcome to Ruby Casino.  Let's play Blackjack."
+    puts ""
+    puts "What's your name?"
+    n = gets.chomp.capitalize
+    puts "Hey there, #{n}! Would you like to play a game of blackjack? Press Any Key to continue"
+    play = gets.chomp
+    if play == ""
+      puts "Ok, the Dealer is ready to deal!"
     end
   end
 
+  def player_cards
+    payer_cards = cards
+    puts "#{n}, you've been dealt #{player_cards}"
+  end
 
   def calculate_total
     hand = cards.map { |e| e[v] }
@@ -45,49 +78,6 @@ class Hand
       else
         total += value.to_i
       end
-    end
-  end
-end
-
-class Player
-  attr_accessor :deck, :cards, :hand
-
-  def initialize(name)
-    @name = name
-    @cards = Array.new
-    put "My cards are #{cards}"
-  end
-end
-
-class Dealer
-  attr_accessor :deck, :cards, :hand
-
-  def initialize
-    @dealer_name = 'Dealer'
-    @cards = Array.new
-  end
-end
-
-# Here are the main game play methods
-class GamePlay
-  attr_accessor :cards, :deck, :hand, :player, :dealer
-
-def initialize
-    @hand = Hand
-    @player = Player
-    @dealer = Dealer
-end
-
-  def intro
-    puts ""
-    puts "=> Welcome to Ruby Casino.  Let's play Blackjack."
-    puts ""
-    puts "What's your name?"
-    name = gets.chomp.capitalize
-    puts "Hey there, #{name}! Would you like to play a game of blackjack? Press Any Key to continue"
-    play = gets.chomp
-    if play == ""
-      puts "Ok, the Dealer is ready to deal!"
     end
   end
 
@@ -109,10 +99,14 @@ end
 
   def run
     intro
-    hand
+    cards
+    deck
     player
+    player_cards
+    dealer
   end
 end
 
 lets_play = GamePlay.new
 lets_play.run
+
